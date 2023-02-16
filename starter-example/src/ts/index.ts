c0d9262b7e97c960151580e8b3bae9eb6dd36e88
sqmasep <leblond.romain@orange.fr>
2023-02-16 12:36:03 +0100

    random stuff

--- starter-example/src/ts/index.ts
+++ starter-example/src/ts/index.ts
@@ -1,1 +1,27 @@
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.fromTo(
+  "#lamp1",
+  {
+    rotateZ: 0,
+    transformOrigin: "top right",
+  },
+  {
+    rotateZ: 50,
+    duration: 5,
+  }
+);
+
+gsap.to(".presentation__line", {
+  clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".presentation__line",
+    scrub: true,
+  },
+});
+
+console.log("jar");
